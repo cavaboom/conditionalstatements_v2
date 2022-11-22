430d30f25172a00b7f9092c6ab459526cf1d6759
dendariev <den.dariev@gmail.com>
2022-11-23 00:04:37 +0800

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,21 @@ public class Main {
         } else  {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        // Задача 2
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2014;
+        if (clientOS == 0) {
+            if (clientDeviceYear > 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear > 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
     }
 }
