e4626657dbbe10bd94754345b98ced152073ba1c
dendariev <den.dariev@gmail.com>
2022-11-23 01:00:37 +0800

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -50,5 +50,31 @@ public class Main {
             deliveryDays++;
         }
         System.out.println("Потребуется дней:" +deliveryDays);
+        // Задача 5
+        System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет");
+        }
     }
 }
